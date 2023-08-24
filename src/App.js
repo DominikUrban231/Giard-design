3ec5cc9fb3cc31129ba831ecbc6d50fcd37ff602
Dominik Urban <dominik.u231@gmail.com>
2023-08-24 21:08:27 +0200

    tailwind working

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App ">
+      <header className="App-header flex">
+        <div className='bg-green-500'>
+          <h1 className='text-5xl'>Witaj Swiecie!</h1>
+          <p>opis krótki</p>
+        </div>
+        <div className='bg-orange-500'>
+        <h1 className='text-3xl font-bold underline'>Witaj Swiecie!</h1>
+          <p>opis krótki</p>
+        </div>
       </header>
     </div>
   );
